f0fda027d372d0e2965f206d9fd321d9dcb4b5b9
Mansour Amine <149618255+Mansour4955@users.noreply.github.com>
2024-10-15 11:03:23 +0200

    just Created CardPopup but still didn't work on it

---
+++ src/components/CardPopup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardPopup = () => {
+  return (
+    <div className='p-2 bg-white absolute left-1/2 -translate-x-1/2 top-5'>CardPopup</div>
+  )
+}
+
+export default CardPopup
